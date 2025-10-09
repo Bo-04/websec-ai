70a632e9eee5a32b563fcaef6742ce889646203e
YousifElsharkawi <700041326@uaeu.ac.ae>
2025-10-10 01:26:02 +0400

    feat: add app router + nextauth route

--- websec-ai/src/lib/auth.ts
+++ websec-ai/src/lib/auth.ts
@@ -2,9 +2,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import GitHubProvider from "next-auth/providers/github";
 import type { NextAuthOptions } from "next-auth";
-
 const prisma = new PrismaClient();
-
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
